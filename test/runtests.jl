a9a820280b98df1024ebb882793883015bcdb631
James Cook <cookjws@gmail.com>
2025-07-14 10:49:45 +0100

    separate factorisation from solve

--- test/runtests.jl
+++ test/runtests.jl
@@ -28,15 +28,16 @@ function concept(;N=4, bs=4, cs=2)
   luAi = [lu!(A) for A in Ai] # each done locally on communicator
   # 2a. Calculate partial solution of A \ C
   C̃i = [luAi[i] \ Ci[i] for i in 1:N] 
-  # 2b. Calculate partial solution of A \ b
-  b̃i = [luAi[i] \ bi[i] for i in 1:N]
-  # 3a. Calculate Schur complement
+  # 3. Calculate Schur complement
   S = D - sum(Bi[i] * C̃i[i] for i in 1:N)
-  # 3b. Calculate right hand side for Schur complement solve
+
+  # 4. Calculate partial solution of A \ b
+  b̃i = [luAi[i] \ bi[i] for i in 1:N]
+  # 5. Calculate right hand side for Schur complement solve
   c̃ = c - sum(Bi[i] * b̃i[i] for i in 1:N)
-  # 4. Solve Schur complement to get lower part of x vector
+  # 6. Solve Schur complement to get lower part of x vector
   y = S \ c̃
-  # 5. Solve for upper parts of x vector
+  # 7. Solve for upper parts of x vector
   x = [b̃i[i] - C̃i[i] * y for i in 1:N]
   return vcat(x..., y), lhs \ rhs
 end
